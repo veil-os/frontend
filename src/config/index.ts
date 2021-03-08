4e35ec437bdf8e873f6d863eae3bd8942ce11484
Raymond Yeh <ray@geek.sg>
2021-03-08 13:52:53 +0800

    fix: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,7 @@
 export const isDevelopment = process.env.NODE_ENV === "development";
-const useLocalCrypto = true;
-const useLocalEndpoint = false;
 
 const generateConfig = () => ({
-  endpoint: useLocalEndpoint ? "http://localhost:3000" : "https://api.veilos.io",
+  endpoint: isDevelopment ? "http://localhost:3000" : "https://api.veilos.io",
   defaults: {
     DEFAULT_EXTERNAL_NULLIFIER: "FOOD_DISTRIBUTION_10022021",
     DEFAULT_IDENTITY_GROUP: "ae883204-758d-412f-b73d-5639401bd8c8",
@@ -11,10 +9,10 @@ const generateConfig = () => ({
   },
   semaphore: {
     treeDepth: 10,
-    provingKeyLocation: useLocalCrypto
+    provingKeyLocation: isDevelopment
       ? "http://localhost:9000/circuit/proving_key.bin"
       : "https://public.veilos.io/circuit/proving_key.bin",
-    circuitLocation: useLocalCrypto
+    circuitLocation: isDevelopment
       ? "http://localhost:9000/circuit/circuit.json"
       : "https://public.veilos.io/circuit/circuit.json",
   },
