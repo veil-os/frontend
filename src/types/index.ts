5cc501da5c9c25426a7d5bee1271b9d8828f5fb6
Raymond Yeh <ray@geek.sg>
2021-03-22 10:53:11 +0800

    feat: identity group management context

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,6 +17,14 @@ export interface InitializedIdentityState {
 
 export type IdentityState = InitializingIdentityState | UninitializedIdentityState | InitializedIdentityState;
 
+export interface IdentityGroupManager {
+  key: string;
+}
+
+export interface IdentityGroupManagers {
+  [identityGroupId: string]: IdentityGroupManager;
+}
+
 export enum AppMode {
   VOUCHER = "VOUCHER",
 }
