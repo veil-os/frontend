140e5ef6e9f1ed66f260116b0fa8a2809ba931a4
Raymond Yeh <ray@geek.sg>
2021-03-22 16:14:14 +0800

    feat: manager key setting modal

--- src/components/ManageGroupContainer/ManageGroupContainer.tsx
+++ src/components/ManageGroupContainer/ManageGroupContainer.tsx
@@ -5,6 +5,7 @@ import { getIdentityCommitmentByGroup, getIdentityGroupInfo } from "../../servic
 import { IdentityCommitments, IdentityGroup } from "../../types";
 import { LayoutDark } from "../Layout";
 import { NavigationBar } from "../NavigationBar";
+import { IdentityGroupManagerModal } from "./IdentityGroupManagerModal";
 
 interface UninitializedIdentitiesState {
   state: "UNINITIALIZED";
@@ -63,8 +64,11 @@ export const Loader: FunctionComponent = () => {
 
 export const ManageGroupContainer: React.FunctionComponent = () => {
   const { identityGroup } = useParams<{ identityGroup: string }>();
+  const [showIdentityManagementModal, setShowIdentityManagementModal] = useState(false);
   const [identityState, setIdentityState] = useState<IdentityGroupsIdentitiesState>({ state: "UNINITIALIZED" });
   const [infoState, setInfoState] = useState<IdentityGroupsInfoState>({ state: "UNINITIALIZED" });
+
+  const toggleIdentityManagementModal = () => setShowIdentityManagementModal(!showIdentityManagementModal);
 
   const loadIdentityCommitments = useCallback(async () => {
     try {
@@ -119,10 +123,31 @@ export const ManageGroupContainer: React.FunctionComponent = () => {
         <div className="bg-white p-4 my-2">
           {infoState.state === "UNINITIALIZED" || (infoState.state === "FETCHING" && <Loader />)}
           {infoState.state === "SUCCESS" && (
-            <>
-              <div className="text-xl text-gray-800">{infoState.identityGroup.name}</div>
-              <div className="text-gray-500">{infoState.identityGroup.identityGroup}</div>
-            </>
+            <div className="flex">
+              <div className="flex-1">
+                <div className="text-xl text-gray-800">{infoState.identityGroup.name}</div>
+                <div className="text-gray-500">{infoState.identityGroup.identityGroup}</div>
+              </div>
+              <div
+                className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-gray-100"
+                onClick={toggleIdentityManagementModal}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6 text-gray-600"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+                  />
+                </svg>
+              </div>
+            </div>
           )}
         </div>
         <div className="text-white text-2xl mt-6 mb-2">Members</div>
@@ -131,6 +156,9 @@ export const ManageGroupContainer: React.FunctionComponent = () => {
           <IdentityCommitmentList identityCommitments={identityState.identityCommitments} />
         )}
       </div>
+      {showIdentityManagementModal && (
+        <IdentityGroupManagerModal identityGroup={identityGroup} toggleModal={toggleIdentityManagementModal} />
+      )}
     </LayoutDark>
   );
 };
