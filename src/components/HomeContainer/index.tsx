3567ab9c9f104dc4df0e9228736b66744bacceb4
Raymond Yeh <ray@geek.sg>
2021-03-24 17:25:00 +0800

    feat: removed unecessary CTA on home

--- src/components/HomeContainer/index.tsx
+++ src/components/HomeContainer/index.tsx
@@ -15,24 +15,6 @@ const HeroSection: React.FunctionComponent = () => {
         <p className="mt-3 max-w-md mx-auto text-base text-gray-300 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Operating System running privacy respecting applications
         </p>
-        <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-          <div className="rounded-md shadow">
-            <a
-              href="#"
-              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-            >
-              Get started
-            </a>
-          </div>
-          <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-            <a
-              href="#"
-              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
-            >
-              Live demo
-            </a>
-          </div>
-        </div>
       </div>
     </main>
   );
